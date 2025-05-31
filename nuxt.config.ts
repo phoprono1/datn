df22f184c24b85a9d31f8f69db9f8c0043d90fbd
phoprono1 <42601200+phoprono1@users.noreply.github.com>
2025-05-31 13:21:12 +0700

    next update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,21 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-
+  hub: {
+    workers: true,
+    database: true
+  },
+  nitro: {
+    preset: 'cloudflare-module'
+  },
   modules: [
-    '@nuxt/content',
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/test-utils',
-    '@nuxt/ui'
+    '@nuxt/ui',
+    '@nuxthub/core'
   ]
 })
